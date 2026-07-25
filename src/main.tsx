a2174a47376db745fb854e678b3272f8b0d93fbc
yajinni <yajinni@gmail.com>
2026-07-25 03:31:50 -0400

    feat: activate draggable sidebar resizing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { installSidebarResize } from "./sidebar-resize";
 import "./styles.css";
 import "./updater.css";
 import "./provider.css";
@@ -10,9 +11,12 @@ import "./sidebar-controls.css";
 import "./sidebar-width.css";
 import "./macos-account-actions.css";
 import "./provider-icon-fixes.css";
+import "./sidebar-resize.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+
+installSidebarResize();
